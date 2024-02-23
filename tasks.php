738a9a63a42ade1fed2bf0df9dadc8513bbfed7c
Denis Utkov <SKNWoolf@mail.ru>
2024-02-23 16:20:14 +0500

    added tasks from lesson 9-19 slide 17

--- tasks.php
+++ tasks.php
@@ -214,6 +214,81 @@
                 ?>
             </div>
         </div>
+        <div class="head"><h1>Урок 9-10 слайд №17</h1></div>
+        <div class="quest">
+            <div class="task"><p><b>1. Создайте функцию, которая принимает одномерный массив и возвращает массив, заполненный случайными числами.</b></p></div>
+            <div class="decision">
+                <?php
+                    function arr($a) {
+                        $b = mt_rand(1,7);
+                            for($i = 0; $i < $b; $i++){
+                            $a[] =+ mt_rand(1,10);
+                            }
+                            return $a;
+                    }
+                    $array = [];
+                    print_r(arr($array));
+                ?>
+            </div>
+        </div>
+        <div class="quest">
+            <div class="task"><p><b>2. Дана строка «HTML, CSS, PHP, BITRIX». Написать функцию, которая определит количество слов строке.</b></p></div>
+            <div class="decision">
+                <?php
+                   $str = 'HTML, CSS, PHP, BITRIX';
+                   function f($a) {
+                       $arr = explode(' ', $a);
+                       $count = 0;
+                       for($i = 0; $i < count($arr); $i++){
+                           $count++;
+                       }
+                       return $count;
+                   }
+                   echo f($str);
+                ?>
+            </div>
+        </div>
+        <div class="quest">
+            <div class="task"><p><b>3.Дана строка «HTML, CSS, PHP, BITRIX». Написать функцию, которая выведет в обратном порядке буквы («XIRTIB ,PHP … »).</b></p></div>
+            <div class="decision">
+                <?php
+                   $str = 'HTML, CSS, PHP, BITRIX';
+                   function reverse($x) {
+                       for($i = mb_strlen($x); $i >= 0; $i--){
+                           echo substr($x, $i, 1);
+                       }
+                   }
+                   echo reverse($str);
+                ?>
+            </div>
+        </div>
+        <div class="quest">
+            <div class="task"><p><b>4. Дана строка «HTML, CSS, PHP, BITRIX». Написать функцию, которая выводит на экран длину строки.</b></p></div>
+            <div class="decision">
+                <?php
+                    $str = 'HTML, CSS, PHP, BITRIX';
+                    function length($a){
+                        echo mb_strlen($a);
+                    }
+                    echo length($str);
+                ?>
+            </div>
+        </div>
+        <div class="quest">
+            <div class="task"><p><b>5. Дана строка «HTML, CSS, PHP, BITRIX». Написать функцию, которая выводит каждую букву на новую строку.</b></p></div>
+            <div class="decision">
+                <?php
+                   $str = 'HTML, CSS, PHP, BITRIX';
+                   function br($a){
+                       for($i = 0; $i < mb_strlen($a); $i++){
+                           echo substr($a, $i, 1) . '<br>';
+                       }
+                       
+                   }
+                   echo br($str);
+                ?>
+            </div>
+        </div>
     </div> 
     <?php require_once 'inc/footer.php' ?>        
 </body>
